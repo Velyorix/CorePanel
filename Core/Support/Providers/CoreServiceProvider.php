dc177dd1ead8380505dd8d550c7375a1cc3be22c
YuketsuSh <sunshineoff1337@gmail.com>
2026-07-15 11:37:54 +0200

    feat(core): register CoreServiceProvider in bootstrap

---
+++ Core/Support/Providers/CoreServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Core\Support\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register Core services and bindings.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap Core services after all providers are registered.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
